0b1610668f14c8a3713574f7abc9fdb7ed0c0714
LuuscoTheLover <127765876+LuuscoTheLover@users.noreply.github.com>
2024-02-12 22:51:31 -0300

    movement and camera halfwaythere

---
+++ Scripts/Camera.gd
@@ -0,0 +1,19 @@
+extends Camera3D
+
+@export var mouse_sense : float = 0.3
+@onready var pivot = $".."
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		pivot.rotate_y(deg_to_rad(-event.relative.x * mouse_sense))
+	
+	if event.is_action_pressed("esc"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event.is_action_pressed("mouse_left"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
