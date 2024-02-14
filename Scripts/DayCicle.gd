3f2998592a040eb56d1ec2d5ec33b1c0b4bd1416
LuuscoTheLover <127765876+LuuscoTheLover@users.noreply.github.com>
2024-02-14 12:47:37 -0300

    fixed daytime added grass textures

--- Scripts/DayCicle.gd
+++ Scripts/DayCicle.gd
@@ -1,46 +1,55 @@
 extends Node
 class_name DayCicleScript
 
+#clock_logic
 var clock_run : bool = false
 var clock : Vector4 = Vector4(0,0,0,0)
 var clock_time : String
+var ten_minute_lenght : float = 0.2
+
 var timer : float = 0.0
+
+#day_sun_logic
 var day_time : float
+var day_lenght : float
+var day_ratio : float
+var start_time : float = 0.35
 
 func _ready():
+	day_lenght = 24 * 6 * ten_minute_lenght
+	day_ratio = 1.0 / day_lenght
+	day_time = start_time
 	start_day()
 	
 func _process(delta):
+	clock_logic()
+	if clock.x == 18:
+		end_day()
+	clock_time = "%02d:%02d" % [clock.x, clock.y]
+
+func clock_logic():
 	if clock_run:
-		clock.z += delta
-	
-		if clock.z >= 0.2:
+		clock.z += get_process_delta_time()
+		if clock.z >= ten_minute_lenght:
 			clock.y += 10
 			clock.z = 0.0
-		
 		if clock.y >= 60:
 			clock.x += 1
 			clock.y = 00
-
 		if clock.x == 24:
 			clock.x = 0
 		
-	if clock.x == 3:
-		end_day()
+		day_time += get_process_delta_time() * day_ratio
 		
-	day_time = clock.x / 24
-	clock_time = "%02d:%02d" % [clock.x, clock.y]
-
 func end_day():
 	clock_run = false
 	timer += get_process_delta_time()
-	print(timer)
 	if timer >= 5.0:
 		timer = 0.0
 		start_day()
 		
-
 func start_day():
 	clock = Vector4(9, 0, 0, clock.w)
 	clock.w += 1
+	day_time = start_time
 	clock_run = true
